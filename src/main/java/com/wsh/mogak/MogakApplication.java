8d3e65db84c24d4ca37fd8f288b7aa0784a9aead
enbraining <me@xolving.com>
2024-02-24 00:18:33 +0900

    Initial commit

---
+++ src/main/java/com/wsh/mogak/MogakApplication.java
@@ -0,0 +1,13 @@
+package com.wsh.mogak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsh/mogak/MogakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsh.mogak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
