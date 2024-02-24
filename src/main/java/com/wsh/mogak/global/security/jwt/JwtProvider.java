bede09c5c124e0f8d1f6bb39fc9e5f489f52685a
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-25 02:46:14 +0900

    add :: JwtProvider 추가

---
+++ src/main/java/com/wsh/mogak/global/security/jwt/JwtProvider.java
@@ -0,0 +1,104 @@
+package com.wsh.mogak.global.security.jwt;
+
+import com.wsh.mogak.global.exception.ErrorCode;
+import com.wsh.mogak.global.exception.GlobalException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtProvider {
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final long ACCESS_TOKEN_TIME = 1000 * 60 * 30;
+    private static final long REFRESH_TOKEN_TIME = 1000 * 60 * 60 * 24 * 7;
+
+    private final Key key;
+
+    public JwtProvider(@Value("${jwt.secret}") String secretKey) {
+
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new GlobalException(ErrorCode.EXPIRED_TOKEN);
+        } catch (Exception e) {
+            throw new GlobalException(ErrorCode.INVALID_TOKEN_TYPE);
+
+        }
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new GlobalException(ErrorCode.INVALID_TOKEN);
+        }
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    private String generateAccessToken(String email) {
+        long now = (new Date()).getTime();
+
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_TIME);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private String generateRefreshToken(UUID id) {
+        long now = (new Date()).getTime();
+
+        Date refreshTokenExpiresIn = new Date(now + REFRESH_TOKEN_TIME);
+
+        return Jwts.builder()
+                .setSubject(id.toString())
+                .setExpiration(refreshTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
