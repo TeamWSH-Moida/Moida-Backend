370137b9866f737b21abf58ee12ad273a7d245e0
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-25 03:35:46 +0900

    delete :: 불필요한 주석 제거

--- src/main/java/com/wsh/mogak/global/security/filter/JwtFilter.java
+++ src/main/java/com/wsh/mogak/global/security/filter/JwtFilter.java
@@ -33,7 +33,6 @@ public class JwtFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    // Request Header 에서 토큰 정보를 꺼내오기
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
--- src/main/java/com/wsh/mogak/global/security/handler/JwtAccessDeniedHandler.java
+++ src/main/java/com/wsh/mogak/global/security/handler/JwtAccessDeniedHandler.java
@@ -14,7 +14,6 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        // 필요한 권한이 없이 접근하려 할때 403
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
