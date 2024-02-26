0854775cb7b8a9f635ec04f51cc8aeb18511e06b
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-27 01:29:06 +0900

    add :: Member Not Found 추가

--- src/main/java/com/wsh/mogak/global/exception/ErrorCode.java
+++ src/main/java/com/wsh/mogak/global/exception/ErrorCode.java
@@ -10,9 +10,13 @@ import org.springframework.http.HttpStatus;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
+    /* Token */
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "Expired Token"),
     INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "Invalid Token Type"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token");
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token"),
+
+    /* Member */
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not Found");
 
     private final HttpStatus status;
     private final String message;
